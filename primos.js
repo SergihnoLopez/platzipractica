8dc46e56031aad0c3b8ba94b9b8646b17ccfefa2
SergihnoLopez <sdlopezs@unal.edu.co>
2019-07-30 19:35:41 -0500

    tratando de hacer js :(

---
+++ primos.js
@@ -0,0 +1,10 @@
+let entero= 4
+console.log("entra0");
+for(let i=2; i<entero; i++){
+    console.log("entra")
+    if (entero%i == 0){
+        console.log("No es primo")
+    }
+
+}
+console.log("sale");
